ce628ed2b580f9acca7a628d8d1850571c674ece
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-08 12:53:15 +0600

    set all the components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import { Button } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar } from 'flowbite-react';
 import './App.css';
 
 function App() {
   return (
-    <div className="bg-primary-400 mt-10 ">
-      <div className='flex text-center align-center'>
-        <p className="text-3xl font-bold text-center text-amber-300">
-          Hello world!
-        </p>
-        <Button
-          outline={true}
-          gradientDuoTone="tealToLime"
-        >
-          Teal to Lime
-        </Button>
-      </div>
+    <div>
+
     </div>
   );
 }
---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthProvider = () => {
+    return (
+        <div>
+            <h1>Auth provider</h1>
+        </div>
+    );
+};
+
+export default AuthProvider;
