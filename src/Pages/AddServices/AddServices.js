28dfdebf61772c68c88b00fa240d8d82c9c2c632
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-09 13:09:54 +0600

    my review and ad service page design

--- src/Pages/AddServices/AddServices.js
+++ src/Pages/AddServices/AddServices.js
@@ -1,3 +1,4 @@
+import { Button, Label, Textarea, TextInput } from 'flowbite-react';
 import React from 'react';
 import useTitle from '../../hooks/useTitle';
 
@@ -5,7 +6,89 @@ const AddServices = () => {
     useTitle('Add Services')
     return (
         <div>
-            <h2>add services</h2>
+            <form className="flex flex-col gap-4 m-12 bg-rose-100 p-4 rounded">
+                <h1 className='text-center pb-4 text-3xl text-blue-900'>You can add any new service you want</h1>
+                <div className='grid grid-cols-2 gap-4'>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="name1"
+                                value="Service Name"
+                            />
+                        </div>
+                        <TextInput
+                            id="name1"
+                            type="text"
+                            placeholder="name"
+                        />
+                    </div>
+
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="image1"
+                                value="Image URL"
+                            />
+                        </div>
+                        <TextInput
+                            id="image1"
+                            type="text"
+                            placeholder="imageURL"
+                        />
+                    </div>
+                </div>
+
+                <div className='grid grid-cols-2 gap-4'>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="rating1"
+                                value="Rating"
+                            />
+                        </div>
+                        <TextInput
+                            id="rating1"
+                            type="text"
+                            placeholder="rating"
+                        />
+                    </div>
+
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="price1"
+                                value="Price"
+                            />
+                        </div>
+                        <TextInput
+                            id="price1"
+                            type="text"
+                            placeholder="price"
+                        />
+                    </div>
+                </div>
+
+
+
+                <div id="textarea">
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="details"
+                            value="Details about this service"
+                        />
+                    </div>
+                    <Textarea
+                        id="details"
+                        placeholder="input short details..."
+                        rows={4}
+                    />
+                </div>
+                <div className='flex justify-center'>
+                    <Button type="submit">
+                        Proceed to Add This Service
+                    </Button>
+                </div>
+            </form>
         </div>
     );
 };
--- src/Pages/MyReview/MyReview.js
+++ src/Pages/MyReview/MyReview.js
@@ -1,11 +1,64 @@
+import { Table } from 'flowbite-react';
 import React from 'react';
+import { HiPencilSquare } from "react-icons/hi2";
+import { HiOutlineTrash } from "react-icons/hi";
 import useTitle from '../../hooks/useTitle';
 
 const MyReview = () => {
     useTitle('My Review')
     return (
-        <div>
-            <h3>My review</h3>
+        <div className='m-12'>
+            <div className='flex justify-center my-4 text-green-500 text-2xl '><p>My review is shown</p></div>
+            <Table hoverable={true}>
+                <Table.Head className='border-y-4'>
+                    <Table.HeadCell className="!p-4">
+                        Delete
+                        {/* <HiOutlineTrash /> */}
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Service Name
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Ratings
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Comments
+                    </Table.HeadCell>
+
+                    <Table.HeadCell>
+                        Edit
+                        <span className="sr-only">
+                            {/* <HiPencilSquare /> */}
+                        </span>
+                    </Table.HeadCell>
+                </Table.Head>
+
+                <Table.Body className="divide-y">
+                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell className="!p-4">
+                            <HiOutlineTrash />
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                            Apple MacBook Pro 17"
+                        </Table.Cell>
+                        <Table.Cell>
+                            Sliver
+                        </Table.Cell>
+                        <Table.Cell>
+                            Laptop
+                        </Table.Cell>
+
+                        <Table.Cell>
+                            <a
+                                href="/tables"
+                                className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                            >
+                                <HiPencilSquare />
+                            </a>
+                        </Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
         </div>
     );
 };
